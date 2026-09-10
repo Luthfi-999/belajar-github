f42cc07552dddb2aea294fe875a74068cd317320
Luthfi <haluthfi007@gmail.com>
2026-09-10 12:00:55 +0700

    Tambah project Python dan C++

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//ini untuk pembuka dari c++ nya
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	
+	//ini variabelnya
+	string a;
+	
+	//cout untuk menampilkan, dan jangan lupa pakai tanda ; untuk penutup
+	//endl untuk longkap satu kebawah biar ga nabrak
+	//cin itu untuk interaksi dengan usernya
+	cout << "Halo Github" << endl;
+	
+	cout << "siapa? " << endl;
+	cin >> a;
+	
+	cout << "oiya halo ", cout << a;
+	
+	//ini harus ada di setiap akhirannya
+	return 0;
+}
---
+++ main.py
@@ -0,0 +1,9 @@
+#print itu menampilkan ke terminal
+print("Halo Github")
+
+#input itu kolom user untuk mengetik
+input("Siapa? ")
+
+#a itu sebagai tempat atau variabel, dan bisa dipanggil
+a = input("Lagi apa? ")
+print("Ohh", a)
