ad78f48e80a6f3ea19d02af5ad46381f6ebbde99
Luthfi <haluthfi007@gmail.com>
2026-09-10 12:16:58 +0700

    Tambah latihan if Python

---
+++ if.py
@@ -0,0 +1,9 @@
+print("cowo atau cewe? ")
+gender = input("pilih: ")
+
+if gender == "cowo":
+    print("Halo kamu cowo ya")
+elif gender == "cewe":
+    print("Halo kamu cewe ya")
+else:
+    print("Kamu gapunhya gender")
