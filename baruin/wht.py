c74beb99b7ac641f0d57cb946b713948d1444c43
Luthfi <haluthfi007@gmail.com>
2026-09-10 12:44:04 +0700

    update

---
+++ baruin/wht.py
@@ -0,0 +1,12 @@
+while True:
+    nama = input("Masukkan Username: ")
+    password = input("Masukkan Password: ")
+    if nama == "upi" and password == "3":
+        print("Login Berhasil")
+        break
+    else:
+        print("Username atau Password salah")
+
+print("====================================")
+print("Selamat Datang Bossss")
+print("====================================")
